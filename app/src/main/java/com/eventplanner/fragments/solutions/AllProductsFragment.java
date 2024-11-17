f86cf7bc58eb8dc1086cbaf8f9c3c01d09a5a2c2
Mrmi <nikola.31023@gmail.com>
2024-11-17 15:55:32 +0100

    Fixed broken all services, events & products fragments

--- app/src/main/java/com/eventplanner/fragments/solutions/AllProductsFragment.java
+++ app/src/main/java/com/eventplanner/fragments/solutions/AllProductsFragment.java
@@ -8,13 +8,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
 
 import com.eventplanner.R;
+import com.eventplanner.adapters.solutions.SolutionListAdapter;
+import com.eventplanner.model.solutions.Product;
+import com.eventplanner.model.solutions.Solution;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllProductsFragment extends Fragment {
 
@@ -24,39 +31,51 @@ public class AllProductsFragment extends Fragment {
         // Inflate the fragment layout
         View rootView = inflater.inflate(R.layout.fragment_all_products, container, false);
 
+        populateList(rootView);
+
         // Set up the filter button
         Button filterButton = rootView.findViewById(R.id.button_filter_products);
-        filterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("AllProductsFragment", "Filter button clicked");
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
-                View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_filter_products, null);
+        filterButton.setOnClickListener(v -> {
+            Log.i("AllProductsFragment", "Filter button clicked");
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+            View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_filter_products, null);
 
-                // Filling filter dialog with radio buttons
-                String[] eventTypes = getResources().getStringArray(R.array.event_types);
-                RadioGroup radioGroupCategories = dialogView.findViewById(R.id.radio_group_categories);
-                for (String eventType : eventTypes) {
-                    RadioButton radioButton = new RadioButton(getActivity());
-                    radioButton.setText(eventType);
-                    radioButton.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
-                    radioGroupCategories.addView(radioButton);
-                }
-
-                // Filling filter dialog with checkboxes
-                LinearLayout eventTypesCheckboxes = dialogView.findViewById(R.id.event_types_checkboxes);
-                for (String eventType : eventTypes) {
-                    CheckBox checkBox = new CheckBox(getActivity());
-                    checkBox.setText(eventType);
-                    checkBox.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
-                    eventTypesCheckboxes.addView(checkBox);
-                }
-
-                bottomSheetDialog.setContentView(dialogView);
-                bottomSheetDialog.show();
+            // Filling filter dialog with radio buttons
+            String[] eventTypes = getResources().getStringArray(R.array.event_types);
+            RadioGroup radioGroupCategories = dialogView.findViewById(R.id.radio_group_categories);
+            for (String eventType : eventTypes) {
+                RadioButton radioButton = new RadioButton(getActivity());
+                radioButton.setText(eventType);
+                radioButton.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
+                radioGroupCategories.addView(radioButton);
             }
+
+            // Filling filter dialog with checkboxes
+            LinearLayout eventTypesCheckboxes = dialogView.findViewById(R.id.event_types_checkboxes);
+            for (String eventType : eventTypes) {
+                CheckBox checkBox = new CheckBox(getActivity());
+                checkBox.setText(eventType);
+                checkBox.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
+                eventTypesCheckboxes.addView(checkBox);
+            }
+
+            bottomSheetDialog.setContentView(dialogView);
+            bottomSheetDialog.show();
         });
 
         return rootView;
+    }
+
+    private void populateList(View rootView) {
+        ListView listView = rootView.findViewById(android.R.id.list);
+
+        List<Solution> solutions = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            solutions.add(new Product((i + 1) + ". Product", "Description for Product " + (i + 1), i * 1000, 0));
+
+        }
+        SolutionListAdapter adapter = new SolutionListAdapter(getContext(), solutions);
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/eventplanner/fragments/solutions/AllServicesFragment.java
+++ app/src/main/java/com/eventplanner/fragments/solutions/AllServicesFragment.java
@@ -8,13 +8,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import androidx.fragment.app.Fragment;
 
 import com.eventplanner.R;
+import com.eventplanner.adapters.solutions.SolutionListAdapter;
+import com.eventplanner.model.solutions.ReservationType;
+import com.eventplanner.model.solutions.Service;
+import com.eventplanner.model.solutions.Solution;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllServicesFragment extends Fragment {
 
@@ -24,39 +32,50 @@ public class AllServicesFragment extends Fragment {
         // Inflate the fragment layout
         View rootView = inflater.inflate(R.layout.fragment_all_services, container, false);
 
+        populateList(rootView);
+
         // Set up the filter button
         Button filterButton = rootView.findViewById(R.id.button_filter_services);
-        filterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("AllServicesFragment", "Filter button clicked");
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
-                View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_filter_services, null);
+        filterButton.setOnClickListener(v -> {
+            Log.i("AllServicesFragment", "Filter button clicked");
+            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
+            View dialogView = getLayoutInflater().inflate(R.layout.bottom_sheet_filter_services, null);
 
-                // Filling filter dialog with radio buttons
-                String[] eventTypes = getResources().getStringArray(R.array.event_types);
-                RadioGroup radioGroupCategories = dialogView.findViewById(R.id.radio_group_categories);
-                for (String eventType : eventTypes) {
-                    RadioButton radioButton = new RadioButton(getActivity());
-                    radioButton.setText(eventType);
-                    radioButton.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
-                    radioGroupCategories.addView(radioButton);
-                }
-
-                // Filling filter dialog with checkboxes
-                LinearLayout eventTypesCheckboxes = dialogView.findViewById(R.id.event_types_checkboxes);
-                for (String eventType : eventTypes) {
-                    CheckBox checkBox = new CheckBox(getActivity());
-                    checkBox.setText(eventType);
-                    checkBox.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
-                    eventTypesCheckboxes.addView(checkBox);
-                }
-
-                bottomSheetDialog.setContentView(dialogView);
-                bottomSheetDialog.show();
+            // Filling filter dialog with radio buttons
+            String[] eventTypes = getResources().getStringArray(R.array.event_types);
+            RadioGroup radioGroupCategories = dialogView.findViewById(R.id.radio_group_categories);
+            for (String eventType : eventTypes) {
+                RadioButton radioButton = new RadioButton(getActivity());
+                radioButton.setText(eventType);
+                radioButton.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
+                radioGroupCategories.addView(radioButton);
             }
+
+            // Filling filter dialog with checkboxes
+            LinearLayout eventTypesCheckboxes = dialogView.findViewById(R.id.event_types_checkboxes);
+            for (String eventType : eventTypes) {
+                CheckBox checkBox = new CheckBox(getActivity());
+                checkBox.setText(eventType);
+                checkBox.setButtonTintList(getResources().getColorStateList(R.color.cool_purple));
+                eventTypesCheckboxes.addView(checkBox);
+            }
+
+            bottomSheetDialog.setContentView(dialogView);
+            bottomSheetDialog.show();
         });
 
         return rootView;
+    }
+
+    private void populateList(View rootView) {
+        ListView listView = rootView.findViewById(android.R.id.list);
+
+        List<Solution> solutions = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            solutions.add(new Service((i + 1) + ". Service", "Description for Service " + (i + 1), i * 1000, 0,
+                    "Specifics for Service", i, i, i, ReservationType.AUTOMATIC));
+        }
+        SolutionListAdapter adapter = new SolutionListAdapter(getContext(), solutions);
+        listView.setAdapter(adapter);
     }
 }
