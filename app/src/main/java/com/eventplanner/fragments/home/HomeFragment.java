3acfbc78d5149cc00ba2aa27a3c10bbaf2aecd6e
Mrmi <nikola.31023@gmail.com>
2024-11-17 15:01:53 +0100

    Updated fragment navigation

--- app/src/main/java/com/eventplanner/fragments/home/HomeFragment.java
+++ app/src/main/java/com/eventplanner/fragments/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +20,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Toast.makeText(getActivity(), "Home frag", Toast.LENGTH_SHORT).show();
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         Button btnFragment1 = view.findViewById(R.id.top_events_button);
