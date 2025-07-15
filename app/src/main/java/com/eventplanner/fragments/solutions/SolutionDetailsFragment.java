3e0b5508ca335dad8271681d5815afdb792e69cc
Danilo Drobnjak <drobnjakdm@gmail.com>
2025-07-15 19:04:20 +0200

    Solution details DTO fixed

--- app/src/main/java/com/eventplanner/fragments/solutions/SolutionDetailsFragment.java
+++ app/src/main/java/com/eventplanner/fragments/solutions/SolutionDetailsFragment.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 import com.eventplanner.R;
 import com.eventplanner.databinding.FragmentSolutionDetailsBinding;
 import com.eventplanner.model.enums.ChatTheme;
+import com.eventplanner.model.enums.DurationType;
 import com.eventplanner.model.requests.chats.CreateChatRequest;
 import com.eventplanner.model.requests.chats.FindChatRequest;
 import com.eventplanner.model.requests.comments.CreateCommentRequest;
@@ -633,7 +634,7 @@ public class SolutionDetailsFragment extends Fragment {
 
     private void populateServiceSpecifics() {
         binding.textSpecifics.setText("Specifics: " + solution.getSpecifics());
-        if (solution.getFixedDurationInSeconds() != null) {
+        if (solution.getDurationType() == DurationType.FIXED) {
             binding.textMinDuration.setVisibility(View.GONE);
             binding.textMaxDuration.setVisibility(View.GONE);
             binding.textDuration.setText(String.format("Duration: %.2f hrs", convertSecondsToHours(solution.getFixedDurationInSeconds())));
